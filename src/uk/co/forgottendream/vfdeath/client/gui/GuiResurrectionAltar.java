478d9d4dd62fc643654ca09d729c80eefef9c6bd
VoodooFrog <voodoofrog@forgottendream.co.uk>
2013-10-14 13:06:14 +0100

    Started work on the text field for the altar gui.

--- src/uk/co/forgottendream/vfdeath/client/gui/GuiResurrectionAltar.java
+++ src/uk/co/forgottendream/vfdeath/client/gui/GuiResurrectionAltar.java
@@ -1,12 +1,17 @@
 package uk.co.forgottendream.vfdeath.client.gui;
 
+import java.util.List;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiTextField;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.inventory.Container;
 import net.minecraft.inventory.ContainerRepair;
 import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
+import net.minecraft.network.packet.Packet250CustomPayload;
 import net.minecraft.util.ResourceLocation;
 
 import org.lwjgl.input.Keyboard;
@@ -15,16 +20,17 @@ import org.lwjgl.opengl.GL11;
 import uk.co.forgottendream.vfdeath.ModInfo;
 import uk.co.forgottendream.vfdeath.config.ConfigHandler;
 import uk.co.forgottendream.vfdeath.inventory.ContainerResurrectionAltar;
+import uk.co.forgottendream.vfdeath.item.ItemResurrectionAnkh;
 import uk.co.forgottendream.vfdeath.tileentity.TileEntityResurrectionAltar;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
 @SideOnly(Side.CLIENT)
-public class GuiResurrectionAltar extends GuiContainer{
+public class GuiResurrectionAltar extends GuiContainer {
 
 	private static final ResourceLocation texture = new ResourceLocation(ModInfo.ID.toLowerCase(), ConfigHandler.GUI_CONTAINER_PATH + "gui_altar.png");
 	private ContainerResurrectionAltar altarContainer;
-	//private GuiTextField playerNameField;
+	private GuiTextField playerNameField;
 	//private InventoryPlayer playerInventory;
 	
 	public GuiResurrectionAltar(InventoryPlayer playerInventory, TileEntityResurrectionAltar altar) {
@@ -43,13 +49,11 @@ public class GuiResurrectionAltar extends GuiContainer{
         Keyboard.enableRepeatEvents(true);
         int i = (this.width - this.xSize) / 2;
         int j = (this.height - this.ySize) / 2;
-        //this.playerNameField = new GuiTextField(this.fontRenderer, i + 62, j + 24, 103, 12);
-        //this.playerNameField.setTextColor(-1);
-        //this.playerNameField.setDisabledTextColour(-1);
-        //this.playerNameField.setEnableBackgroundDrawing(false);
-        //this.playerNameField.setMaxStringLength(40);
-        //this.inventorySlots.removeCraftingFromCrafters(this);
-        //this.inventorySlots.addCraftingToCrafters(this);
+        this.playerNameField = new GuiTextField(fontRenderer, guiLeft + 51, guiTop + 58, 103, 12);
+        this.playerNameField.setTextColor(-1);
+        this.playerNameField.setDisabledTextColour(-1);
+        this.playerNameField.setEnableBackgroundDrawing(false);
+        this.playerNameField.setMaxStringLength(40);
     }
 	
 	@Override
@@ -57,68 +61,68 @@ public class GuiResurrectionAltar extends GuiContainer{
     {
         super.onGuiClosed();
         Keyboard.enableRepeatEvents(false);
-        //this.inventorySlots.removeCraftingFromCrafters(this);
     }
 	
-	/*@Override
-    protected void drawGuiContainerForegroundLayer(int par1, int par2)
+	@Override
+    protected void mouseClicked(int par1, int par2, int par3)
     {
+        super.mouseClicked(par1, par2, par3);
+        this.playerNameField.mouseClicked(par1, par2, par3);
+        /*if(playerNameField.isFocused() && playerNameField.getText() == "Player Name") {
+        	playerNameField.setText("");
+        }*/
+    }
+	
+    private int getChargedAnkhCount() {
+		int count = 0;
+		
+		for(int x = 0; x < 10; x++) {
+			ItemStack item = altarContainer.getSlot(x + 36).getStack();
+
+			if(item != null) {
+				if(item.getItem() instanceof ItemResurrectionAnkh) {
+					ItemResurrectionAnkh ankh = (ItemResurrectionAnkh) item.getItem();
+
+					if (ankh.hasEffect(item)) {
+						count++;
+					}
+				}
+			}
+		}
+		return count;
+    }
+    
+    @Override
+    public void drawScreen(int par1, int par2, float par3)
+    {
+        super.drawScreen(par1, par2, par3);
         GL11.glDisable(GL11.GL_LIGHTING);
-        this.fontRenderer.drawString(I18n.getString("container.repair"), 60, 6, 4210752);
-        
-        int k = 8453920;
-        boolean flag = true;
-        String s = I18n.getStringParams("container.repair.cost");
-
-        if (!this.altarContainer.getSlot(2).getHasStack()) {
-        	flag = false;
-        }
-        else if (!this.altarContainer.getSlot(2).canTakeStack(this.playerInventory.player)) {
-        	k = 16736352;
-        }
-
-        if (flag) {
-        	int l = -16777216 | (k & 16579836) >> 2 | k & -16777216;
-        	int i1 = this.xSize - 8 - this.fontRenderer.getStringWidth(s);
-        	byte b0 = 67;
-
-        	if (this.fontRenderer.getUnicodeFlag()) {
-        		drawRect(i1 - 3, b0 - 2, this.xSize - 7, b0 + 10, -16777216);
-        		drawRect(i1 - 2, b0 - 1, this.xSize - 8, b0 + 9, -12895429);
-        	} else {
-        		this.fontRenderer.drawString(s, i1, b0 + 1, l);
-        		this.fontRenderer.drawString(s, i1 + 1, b0, l);
-        		this.fontRenderer.drawString(s, i1 + 1, b0 + 1, l);
-        	}
-
-        	this.fontRenderer.drawString(s, i1, b0, k);
-        }
-
-        GL11.glEnable(GL11.GL_LIGHTING);
-    }*/
-
+        this.playerNameField.drawTextBox();
+    }
+    
 	@Override
 	protected void drawGuiContainerBackgroundLayer(float f, int i, int j) {
 		GL11.glColor4f(1, 1, 1, 1);
 		Minecraft.getMinecraft().getTextureManager().bindTexture(texture);
 		drawTexturedModalRect(guiLeft, guiTop, 0, 0, xSize, ySize);
-		
-		boolean hasAnkh = false;
-		
-		//TODO: Fix this
-		for(int x = 0; x < 10; x++) {
-			if(altarContainer.getSlot(x + 36).getHasStack()) {
-				if(altarContainer.getSlot(x + 36).getStack().itemID == ConfigHandler.RES_ANKH_ID) {
-					hasAnkh = true;
-				} else {
-					hasAnkh = false;
-				}
-			} else {
-				hasAnkh = false;
-			}
-		}
-		
-		drawTexturedModalRect(guiLeft + 51, guiTop + 58, 0, this.ySize + (hasAnkh ? 0 : 16), 110, 16);
+
+		drawTexturedModalRect(guiLeft + 51, guiTop + 58, 0, this.ySize + (getChargedAnkhCount() > 0 ? 0 : 16), 110, 16);
 	}
+	
+	//TODO: Need to find a better way to update the text field and it's contents
+	@Override
+    public void updateScreen()
+    {
+        super.updateScreen();
+        
+        if (getChargedAnkhCount() > 0)
+        {
+            this.playerNameField.setText("Player Name");
+            this.playerNameField.setEnabled(true);
+        } else {
+            this.playerNameField.setText("");
+            this.playerNameField.setEnabled(false);
+        }
+    }
 
 }
