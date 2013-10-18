bfc6ca190e2bef16dce6359da76b48bac53edfb2
VoodooFrog <voodoofrog@forgottendream.co.uk>
2013-10-18 14:58:59 +0100

    Added event handler for ghost mode.

--- src/uk/co/forgottendream/vfdeath/config/ConfigHandler.java
+++ src/uk/co/forgottendream/vfdeath/config/ConfigHandler.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.util.UUID;
 
 import net.minecraftforge.common.Configuration;
+import net.minecraftforge.common.Property;
 import uk.co.forgottendream.vfdeath.ModInfo;
 
 public class ConfigHandler {
@@ -33,14 +34,16 @@ public class ConfigHandler {
 	//General
 	public static int RES_ANKH_XP_COST; //Default: 10
 	public static int HEART_LOSS_ON_DEATH; //Default: 1
+	public static Property GHOST_ALLOWED_BLOCKS;
 	
 	
 	public static void initialize(File file) {
 		Configuration config = new Configuration(file);
 		
 		config.load();
-		HEART_LOSS_ON_DEATH = config.get(generalcat, "HeartLossOnDeath", 1).getInt();
-		RES_ANKH_XP_COST = config.get(generalcat, "ResAnkhXPCost", 10).getInt();
+		HEART_LOSS_ON_DEATH = config.get(generalcat, "HeartLossOnDeath", 1, "How many hearts a player loses on death").getInt();
+		RES_ANKH_XP_COST = config.get(generalcat, "ResAnkhXPCost", 10, "Cost in player levels to charge an ankh by one point").getInt();
+		GHOST_ALLOWED_BLOCKS = config.get(generalcat, "GhostAllowedBlocks", "", "Comma seperated list of block ids (no metadata) that you can interact with as a ghost.");
 		RES_ANKH_ID = config.getItem(itemidscat, "ResAnkh", 17070).getInt() -256;
 		ALTAR_ID = config.getBlock(blockidscat, "ResAltar", 1700).getInt();
 		
---
+++ src/uk/co/forgottendream/vfdeath/eventhandlers/EventHandlerGhost.java
@@ -0,0 +1,95 @@
+package uk.co.forgottendream.vfdeath.eventhandlers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.minecraft.block.Block;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.potion.Potion;
+import net.minecraftforge.event.ForgeSubscribe;
+import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent.Action;
+import uk.co.forgottendream.vfdeath.config.ConfigHandler;
+
+public class EventHandlerGhost {
+	public static List<Integer> allowedBlockIDs = new ArrayList();
+
+	@ForgeSubscribe
+	public void disableDamage(LivingHurtEvent event) {
+		if (event.source.getEntity() instanceof EntityPlayer) {
+			EntityPlayer attackingPlayer = (EntityPlayer) event.source.getEntity();
+			NBTTagCompound compound = attackingPlayer.getEntityData().getCompoundTag("VFDeathPlayerData");
+
+			if (compound.getBoolean("IsDead") && !attackingPlayer.capabilities.isCreativeMode) {
+				event.setCanceled(true);
+			}
+		}
+	}
+
+	@ForgeSubscribe
+	public void disableInteraction(PlayerInteractEvent event) {
+		EntityPlayer player = event.entityPlayer;
+		NBTTagCompound compound = player.getEntityData().getCompoundTag("VFDeathPlayerData");
+
+		if (compound.getBoolean("IsDead") && !player.capabilities.isCreativeMode) {
+			int blockID = player.worldObj.getBlockId(event.x, event.y, event.z);
+
+			if (allowedBlockIDs == null) {
+				buildAllowedBlocks();
+			}
+
+			if (!allowedBlockIDs.contains(Integer.valueOf(blockID))) {
+				event.setCanceled(true);
+			}
+
+			if (event.action != Action.LEFT_CLICK_BLOCK	&& player.getHeldItem() != null	&& player.getHeldItem().itemID == Item.appleGold.itemID) {
+				event.setCanceled(false);
+			}
+		}
+	}
+
+	@ForgeSubscribe
+	public void onEntityUpdate(LivingUpdateEvent event) {
+		if (event.entityLiving instanceof EntityPlayer) {
+			EntityPlayer player = (EntityPlayer) event.entityLiving;
+			NBTTagCompound nbt = player.getEntityData().getCompoundTag(
+					"PlayerPersisted");
+
+			if (nbt.getBoolean("isDead") && !player.capabilities.isCreativeMode) {
+				int invisTimeLeft = player.getActivePotionEffect(
+						Potion.invisibility).getDuration();
+				if (invisTimeLeft < 100) {
+					player.getActivePotionEffect(Potion.invisibility).duration = 1000;
+					// player.addChatMessage("adding time");
+				}
+			}
+		}
+	}
+
+	public static void buildAllowedBlocks() {
+		String allowedBlocks = ConfigHandler.GHOST_ALLOWED_BLOCKS.getString();
+		allowedBlockIDs.add(Integer.valueOf(Block.doorWood.blockID));
+		allowedBlockIDs.add(Integer.valueOf(Block.trapdoor.blockID));
+		allowedBlockIDs.add(Integer.valueOf(Block.fenceGate.blockID));
+		allowedBlockIDs.add(Integer.valueOf(Block.lever.blockID));
+		allowedBlockIDs.add(Integer.valueOf(Block.stoneButton.blockID));
+		allowedBlockIDs.add(Integer.valueOf(Block.woodenButton.blockID));
+
+		if (allowedBlocks != null) {
+			String[] blockArray = allowedBlocks.split(",");
+
+			for (int i = 0; i < blockArray.length; i++) {
+				String blockID = blockArray[i];
+
+				try {
+					blockID = blockID.trim();
+					allowedBlockIDs.add(Integer.valueOf(Integer.parseInt(blockID)));
+				} catch (Exception var6) {}
+			}
+		}
+	}
+}
